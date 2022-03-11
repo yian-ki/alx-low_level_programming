9d12b734bfdcc949c647d52ecd689eaf3e79830c
yian-ki <akinyi.laurine@students.jkuat.ac.ke>
2022-03-11 09:16:21 -0800

    Find last digit and compare

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return:1
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int last = n % 10;
+	if (last > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %i is %i and is 0\n", n, last);
+	else
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+	return (0);
+}
