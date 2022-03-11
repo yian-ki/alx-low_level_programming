8c1600c50aa3ceca217a0c0210a99e55a61e860d
yian-ki <akinyi.laurine@students.jkuat.ac.ke>
2022-03-11 09:18:36 -0800

    Remove declaration in code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,12 +11,12 @@
 
 int main(void)
 {
-	int n;
+	int n, last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last = n % 10;
+	last = n % 10;
 	if (last > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, last);
 	else if (last == 0)
