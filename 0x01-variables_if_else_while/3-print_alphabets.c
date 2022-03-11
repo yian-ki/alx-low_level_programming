2e0ad46def398d3569bd875540baae2023c57398
yian-ki <akinyi.laurine@students.jkuat.ac.ke>
2022-03-11 09:39:02 -0800

    Print alphabet in lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:1
+ */
+
+int main(void)
+{
+char c = 'a'
+while (c <='z')
+{
+	putchar(c);
+	c++;
+}
+char d = 'A';
+while ( d <='Z')
+{
+	putchar(d);
+	d++;
+}
+putchar('\n');
+}
