1760034ffe4e2fcfd66f78f322cdd3b2558c16c6
yian-ki <akinyi.laurine@students.jkuat.ac.ke>
2022-03-11 05:40:58 -0800

    Prints with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+/*Program to print using puts*/
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
