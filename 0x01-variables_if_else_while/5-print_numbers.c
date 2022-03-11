1ef27614de40055f5d41f8427d841f7476913c79
yian-ki <akinyi.laurine@students.jkuat.ac.ke>
2022-03-11 10:05:48 -0800

    Print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a = 0;
+
+while (a < 10)
+{
+printf("%i", a);
+a++;
+}
+
+return (0);
+}
