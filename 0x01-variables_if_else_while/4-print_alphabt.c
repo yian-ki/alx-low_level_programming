b12d588d5d0d26650e02ac2121e262cf71d46985
yian-ki <akinyi.laurine@students.jkuat.ac.ke>
2022-03-11 09:53:24 -0800

    Print alphabet except some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:1
+ */
+
+int main(void)
+{
+char c = 'a';
+
+while (c <= 'z')
+{
+	if (c == 'q' || c=='e')
+		continue;
+        putchar (c);
+        c++;
+}
+putchar('\n');
+return (0);
+}
