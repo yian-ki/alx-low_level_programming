19659db002cef1aded85572825ad30e3da9fe831
yian-ki <akinyi.laurine@students.jkuat.ac.ke>
2022-03-11 10:28:15 -0800

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a = 0;
+char c = 'a';
+
+while (a < 10)
+{
+putchar(a + '0');
+a++;
+}
+
+while (c <= f)
+{
+putchar(c);
+c++;
+}
+
+putchar('\n');
+
+return (0);
+}
